5bf70cd7e1ae2aad3752dfe2778ba4dab5e78c43
AkshatOdiya <akshatodiya79@gmail.com>
2025-08-01 22:00:34 +0530

    Modified

--- src/MerkleAirdrop.sol
+++ src/MerkleAirdrop.sol
@@ -3,12 +3,15 @@ pragma solidity ^0.8.20;
 
 import {IERC20, SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import {MerkleProof} from "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
+import {EIP712} from "@openzeppelin/contracts/utils/cryptography/EIP712.sol";
+import {ECDSA} from "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 
-contract MerkleAirdrop {
+contract MerkleAirdrop is EIP712 {
     using SafeERC20 for IERC20;
 
     error MerkleAirdrop__InvalidProof();
     error MerkleAirdrop__AccountHasAlreadyClaimedOnce();
+    error MerkleAirdrop__InvalidSignature();
 
     event AccountClaimed(address indexed user, uint256 indexed amount);
 
@@ -17,14 +20,26 @@ contract MerkleAirdrop {
     IERC20 private immutable i_airdropToken;
     mapping(address claimer => bool hasClaimed) private s_checkClaimed;
 
-    constructor(bytes32 merkleRoot, IERC20 airdropToken) {
+    bytes32 private constant MESSAGE_TYPEHASH = keccak256("AirdropClaim(address account,uint256 amount)");
+
+    struct AirdropClaim {
+        address account;
+        uint256 amount;
+    }
+
+    constructor(bytes32 merkleRoot, IERC20 airdropToken) EIP712("MerkleAirdrop", "1") {
         i_merkleRoot = merkleRoot;
         i_airdropToken = airdropToken;
     }
 
-    function claim(address account, uint256 amount, bytes32[] calldata merkleProof) external {
+    function claim(address account, uint256 amount, bytes32[] calldata merkleProof, uint8 v, bytes32 r, bytes32 s)
+        external
+    {
         if (s_checkClaimed[account]) {
             revert MerkleAirdrop__AccountHasAlreadyClaimedOnce();
+        }
+        if (!_isValidSignature(account, getMessage(account, amount), v, r, s)) {
+            revert MerkleAirdrop__InvalidSignature();
         }
         // When we are using merkle proofs, hashing two times prevents collision -->  Second Preimage Attack
         bytes32 leaf = keccak256(bytes.concat(keccak256(abi.encode(account, amount))));
@@ -37,11 +52,25 @@ contract MerkleAirdrop {
         emit AccountClaimed(account, amount);
     }
 
+    function getMessage(address account, uint256 amount) public view returns (bytes32) {
+        return
+            _hashTypedDataV4(keccak256(abi.encode(MESSAGE_TYPEHASH, AirdropClaim({account: account, amount: amount}))));
+    }
+
     function getMerkleRoot() external view returns (bytes32) {
         return i_merkleRoot;
     }
 
     function getAirdropToken() external view returns (IERC20) {
         return i_airdropToken;
+    }
+
+    function _isValidSignature(address account, bytes32 digest, uint8 v, bytes32 r, bytes32 s)
+        internal
+        pure
+        returns (bool)
+    {
+        (address actualSigner,,) = ECDSA.tryRecover(digest, v, r, s);
+        return actualSigner == account;
     }
 }
